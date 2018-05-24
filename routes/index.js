e0a9d79d7c9a82c684a5a15d63fd880c0b3a16be
ChangerHe <244737162@qq.com>
2018-05-24 19:14:32 +0800

    feat: 增加socket.io的demo

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,14 @@
  * @Author: ChangerHe 
  * @Date: 2018-05-17 23:26:21 
  * @Last Modified by: ChangerHe
- * @Last Modified time: 2018-05-21 13:13:08
+ * @Last Modified time: 2018-05-24 18:14:11
  */
 
 var express = require('express');
 var {User, Test} = require('../models/index')
 var router = express.Router();
 
-router.get('/', async (req, res, next) => {
+router.get('/db', async (req, res, next) => {
   // const user = await User.findOne()
   const user = await User.findById('4b67b598-a5f9-4401-a04a-31505a2dccc2')
   const test = await Test.findOne()
@@ -20,5 +20,32 @@ router.get('/', async (req, res, next) => {
 router.get('/home', function(req, res, next) {
   res.render('index', {title: 'test'})
 })
+
+router.get('/', function (req, res) {
+  if (req.cookies.user == null) {
+    res.render('signin', {title: 'test'})
+  } else {
+    res.render('index', {title: 'test'})
+  }
+});
+router.get('/signin', function (req, res) {
+  res.render('signin', {title: 'test'})
+});
+router.post('/signin', async function (req, res) {
+  const user = await User.findAll({
+    where: {
+      nickname: req.body.name
+    }
+  })
+  console.log(user, 'user')
+  if (user.length) {
+    //存在，则不允许登陆
+    res.redirect('/signin');
+  } else {
+    //不存在，把用户名存入 cookie 并跳转到主页
+    res.cookie("user", req.body.name, {maxAge: 1000*60*60*24*30});
+    res.redirect('/');
+  }
+});
 
 module.exports = router;
